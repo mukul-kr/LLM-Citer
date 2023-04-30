8a4360dfba0c6b5b19fff218898aa84b7b6adf65
mstomar698 <tomarm698@gmail.com>
2023-04-30 23:03:17 +0530

    [4]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import { Home } from './components/Home';
 import FormData from './components/FormData';
-import WrongPage from './components/WrongPage';
+import WrongPage from './screens/WrongPage';
+import SignInScreen from './screens/SignInScreen';
 
 function App() {
   return (
@@ -14,10 +15,22 @@ function App() {
           Cite AI
         </Typography>
         <Routes>
+          <Route path="/signin" element={<SignInScreen />} />
+          {/**
         <Route path="/cites/:id" element={<FormData />} />
-        <Route path="/cites" element={<WrongPage />} />
+      */}
+
+          <Route path="/cites/:id" element={<FormData />}></Route>
+          <Route path="/cites" element={<WrongPage />} />
           <Route path="/" element={<Home />} />
         </Routes>
+        <btn
+          onClick={() =>
+            (window.location.href = 'https://llm-citer-one.vercel.app/cites')
+          }
+        >
+          direct to cites page
+        </btn>
       </Container>
     </BrowserRouter>
   );
